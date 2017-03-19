a85c68de28d297840d0705f1b6f3f9095ff8a3f2
kekearif <kekearif@googlemail.com>
2017-03-19 19:13:08 +0800

    ex4

--- hello.py
+++ hello.py
@@ -2,24 +2,50 @@ from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'sfd7868732hfayf92'
 # The __name__ argument is used for flask to determine the root path of the app
 # Note how the ext is init the same as the others, passing the app in
 # This moment object is now available in templates
+# The config is a dictionary. sec key is a config var used by flask and exts
+# First wtf import is the flask ext
+# The other two are from the wtforms package
 
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+# GET is the default if we provide dict need to add get if need it
+# Here we need a dict for the POST method on the form
+@app.route('/', methods=['GET ', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    # Check all validators are ok
+    if form.validate_on_submit:
+        name = form.name.data  # When submit set the name
+        form.name.data = ''    # Clear the field in the form
+    return render_template('index.html', name=name, form=form)
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.route('/shitform')
+def shitform():
+    form = NameForm()
+    return render_template('shitform.html', form=form)
 
 
 @app.errorhandler(404)
